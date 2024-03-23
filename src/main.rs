70d0a3d287b2fcb4ccaef087193d4ca300cea0ed
johnlin <john830110@gmail.com>
2024-03-24 00:26:26 +0800

    feat: 新增檢查 store 和 wants

--- src/main.rs
+++ src/main.rs
@@ -61,6 +61,9 @@ fn main() -> io::Result<()> {
     let clues = get_input_clues()?;
     println!("{:?}", clues);
 
+    let n_players = players.len();
+
+    // create player stores and wants
     let mut all_stores = Vec::new();
     let mut all_wants = Vec::new();
 
@@ -75,6 +78,43 @@ fn main() -> io::Result<()> {
     println!("{:?}", all_stores);
     println!("{:?}", all_wants);
 
+    // check stores and wants
+    for (i, player_stores) in all_stores.iter().enumerate() {
+        for (j, &store) in player_stores.iter().enumerate() {
+            // change want to 1 if store >= 3
+            if store >= 3 {
+                all_wants[i][j] = 1;
+            }
+
+            // change want to -1 if store is 0
+            if store == 0 {
+                if all_wants[i][j] == 1 {
+                    println!("Player {} does not have clue {}, cannot exchange with others", i + 1, j + 1);
+                }
+                all_wants[i][j] = -1;
+            }
+        }
+    }
+
+    for i in 0..NUM_CLUES {
+        let mut total_players_with_clue = 0;
+        for j in 0..n_players {
+            if all_stores[j][i] > 0 {
+                total_players_with_clue += 1;
+            }
+        }
+
+        if total_players_with_clue < 2 {
+            println!("線索{}的玩家數量不足", i + 1);
+            for j in 0..n_players {
+                all_wants[j][i] = -1;
+            }
+        }
+    }
+
+    println!("===================== After checking =====================");
+    println!("{:?}", all_stores);
+    println!("{:?}", all_wants);
 
     Ok(())
 }
